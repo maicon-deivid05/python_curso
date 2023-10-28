e388ba9c59494305a85c05e778aed6cb7cb8a104
Maicon deivid <maiconlifee@gmail.com>
2023-10-28 07:31:13 +0000

    EAFP

--- hello.py
+++ hello.py
@@ -30,17 +30,28 @@ arguments = {
     "count": 1,
 }
 for arg in sys.argv [1:]:
-    key, value = arg.split("=")
+    try:
+        key, value = arg.split("=")
+    except ValueError as e:
+        #TODO : logging
+        print(f"[ERROR] {str(e)}")
+        print("You need to use `=`")
+        print(f"You passed {arg}")
+        print("try with --key=value")
+        sys.exit(1)
     key = key.lstrip("-").strip()
     value = value.strip()
+
+    #Validação
     if key not in arguments:   
         print(f"invalid Option `{key}`")
         sys.exit()
+
     arguments[key] = value
- 
 
 current_language = arguments["lang"]
 if current_language is None:
+     #TODO : Usar repetição
     if "LANG" is os.environ:
         current_language = os.getenv("LANG")
     else:
@@ -56,4 +67,11 @@ msg = {
     "fr_FR": "Bonjour, Monde!",
 }
 
+#EAFP
+try:
+    message = msg[current_language]
+except KeyError as e:
+    print(f"[ERROR] {str(e)}")
+    print(f"Language is invalid, choose from: {list(msg.keys())}")
+    sys.exit(1)
 print(msg[current_language] * int(arguments["count"]))  
